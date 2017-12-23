09defa7902258b3162b0f0dfc270221037488129
beauvelop <beau.html@gmail.com>
2017-12-22 23:45:30 -0500

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+"use strict";
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require("gulp-rename"),
+  sass = require('gulp-sass'),
+  maps = require('gulp-sourcemaps'),
+  del = require('del'),
+  gutil = require('gulp-util');
+
+gulp.task('concatScripts', () =>
+  gulp.src(['js/jquery-3.2.1.min.js',
+            'js/tweenMax.min.js',
+            'js/stars.js',
+            'js/secondJS.js',
+            'js/thirdJS.js'])
+    .pipe(concat('app.js')) //takes str that will name concatted file
+    .pipe(gulp.dest('./build/js'))
+);
+
+gulp.task('minifyScripts', ["concatScripts"], () =>
+  gulp.src('build/js/app.js')
+    .pipe(uglify().on('error', gutil.log)) //this logs errors)
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('./build/js')
+    )
+)
+
+gulp.task('compileSass', () =>
+  gulp.src('scss/master.scss')
+    .pipe(maps.init())
+    .pipe(sass())
+    .pipe(rename('styles.css'))
+    .pipe(maps.write('./')) 
+    .pipe(gulp.dest('./build/css')
+    )
+)
+
+gulp.task('watchSass', () =>
+  gulp.watch(["scss/**/*.scss"], ["compileSass"])
+  // "**" -- look for any folder
+  // "*.scss" -- watch any SASS file
+)
+
+gulp.task('clean', () =>
+  del(['dist', 'build']),
+  console.log("Deleting 'dest' and 'dist' folders")
+)
+
+gulp.task("build", ['minifyScripts', 'compileSass'], () =>
+  gulp.src(
+    [
+      'build/css/styles.css',
+      'build/js/app.min.js',
+      'index.html',
+      'img/**'
+    ],
+    { base: './' } // maintains directory tree integrity and passes into dist folder
+  )
+    .pipe(gulp.dest('dist'))
+);
+
+gulp.task("default", ["clean"], () =>
+  gulp.start("build")
+);
---
+++ js/stars.js
@@ -0,0 +1,66 @@
+(function(){
+    var twinkleBtn = $('#twinkleBtn');
+    
+    var star1 = $('#star1'),
+      star2 = $('#star2'),
+      star3 = $('#star3'),
+      star4 = $('#star4'),
+      star5 = $('#star5'),
+      star6 = $('#star6'),
+      star7 = $('#star7'),
+      star8 = $('#star8'),
+      star9 = $('#star9'),
+      star10 = $('#star10'),
+      star11 = $('#star11'),
+      star12 = $('#star12'),
+      star13 = $('#star13'),
+      star14 = $('#star14'),
+      star15 = $('#star15'),
+      star16 = $('#star16'),
+      star17 = $('#star17');
+      
+    /***********************************/
+      var tl = new TimelineMax({
+      onReverseComplete:reverseRepeat,
+      onReverseCompleteParams:['{self}'],
+      onComplete:complete,
+      onCompleteParams:['{self}']
+    });
+    
+    var array = [star1,star2,star3,star4,star5,star6,star7,star8, star9, star10, star11, star12, star13, star14, star15, star16, star17];
+   // get collection of children  
+    // array.forEach(item => tl.to(item, 1, {alpha: 0}).to(item, 1, {alpha: 1})) //ES6
+    /* using pre es6 for gulp */
+    array.forEach(function(item) { 
+        tl.to(item, 1, {alpha: 0}).to(item, 1, {alpha: 1});    
+    })
+
+    function reverseRepeat(tl) {
+      tl.reverse(0); // 0 sets the playhead at the end of the animation
+    }
+    function complete(tl) {
+      tl.restart(); // 0 sets the playhead at the end of the animation
+    }
+    
+    twinkleBtn.on("click", function() {
+      tl.play();
+      tl.reversed(!tl.reversed());
+    })
+    tl.pause();
+    
+    /***********************************/
+    
+      
+      
+    //   var IDs = $("#stars path")         // find all paths
+    //   .map(function() { return this.id; }) // convert to set of IDs
+    
+    //   var nuArr=[];
+      
+    //   IDs.map((item, idx) => nuArr.push(IDs[item]))
+    // console.log(nuArr)  
+    
+      
+      
+    
+    }());
