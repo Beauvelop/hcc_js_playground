4ea4b0d748f00b2e1c2a27a26071b2af181a5680
beauvelop <beau.html@gmail.com>
2017-12-26 01:54:09 -0500

    refactored and erased code governing the link of btns to corresponding SVGS

--- js/general.js
+++ js/general.js
@@ -1,40 +1,17 @@
 ~function() {
-    var btns = $('button');
-    var landBtn = $('#landBtn');
-    var ufoBtn = $('#ufoBtn');
-    var moonBtn = $('#moonBtn');
-    var houseBtn = $('#houseBtn');
-    var treeBtn = $('#treeBtn');
-    var starsBtn = $('#starsBtn');
-    
-    starsBtn.on("click", function() {
-        console.log("You clicked this: ", this, "\n with an id of ", this.id);
-        $('#twinkleBtn').toggle('fast');
-        $('#starsSVG').toggle('slow');
-    });
-    landBtn.on("click", function() {
-        console.log("You clicked this: ", this, "\n with an id of ", this.id);
-        $('#landscape').toggle('slow');
-    });
-    ufoBtn.on("click", function() {
-        console.log("You clicked this: ", this, "\n with an id of ", this.id);
-        $('#alien').toggle('slow');
-    });
-    moonBtn.on("click", function() {
-        console.log("You clicked this: ", this, "\n with an id of ", this.id);
-        $('#moon').toggle('slow');
-    });
-    houseBtn.on("click", function() {
-        console.log("You clicked this: ", this, "\n with an id of ", this.id);
-        $('#house').toggle('slow');
-    });
-    treeBtn.on("click", function() {
-        console.log("You clicked this: ", this, "\n with an id of ", this.id);
-        $('#tree').toggle('slow');
-    });
-    
-    // btns.each().on("click",function() {
-    //         alert(this);
-    //     })
-        
+    var landBtn = $('#landBtn'),
+        ufoBtn = $('#ufoBtn'),
+        moonBtn = $('#moonBtn'),
+        houseBtn = $('#houseBtn'),
+        treeBtn = $('#treeBtn'),
+        starsBtn = $('#starsBtn');
+
+    var svgArr = [landBtn, starsBtn, ufoBtn, treeBtn, houseBtn, moonBtn];
+
+    svgArr.forEach(function(btn) {
+        btn.on("click", function() {
+            var linkedSVG = "#"+ this.id.toString().slice(0, -3)+"SVG"; //create str for id of corresponding SVG
+            $(linkedSVG).toggle("slow");
+        })
+    })
 }();
